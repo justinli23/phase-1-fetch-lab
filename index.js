7fb6e7ea13b691d2934bf17605dc6ff861d68ef3
Justin Li <justinlit23@gmail.com>
2022-04-13 13:43:45 -0700

    commit tree dcfce3adbfdeeb828bcff2a6cf2207dec17272f6

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((response) => response.json())
+  .then((json) => renderBooks(json));
+  return renderBooks(json)
   // To pass the tests, don't forget to return your fetch!
-  
 }
 
 function renderBooks(books) {
